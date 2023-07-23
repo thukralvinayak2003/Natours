306abf38d239366c7197fd9cb02606227c2fcf38
thukralvinayak2003 <100865143+thukralvinayak2003@users.noreply.github.com>
2023-07-23 19:34:01 +0530

    Deployment ready

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,8 +45,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const url = `${req.protocol}://${req.get('host')}/me`;
-  await new Email(newUser, url).sendWelcome();
+  // const url = `${req.protocol}://${req.get('host')}/me`;
+  // await new Email(newUser, url).sendWelcome();
   // it gives a promise
   createSendToken(newUser, 201, res);
 });
