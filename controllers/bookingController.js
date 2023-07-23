f0ebe6cd04f6e72e69d5589e60d67a06142e73be
thukralvinayak2003 <100865143+thukralvinayak2003@users.noreply.github.com>
2023-07-23 17:49:30 +0530

    Stripe ready

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -23,7 +23,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   const price = await stripe.prices.create({
     product: product.id,
-    unit_amount: tour.price * 100,
+    unit_amount: tour.price * 1000,
     currency: 'inr',
   });
 
@@ -66,7 +66,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email }).id; //get the userId
-  const { price } = session.line_items[0];
+  const { price } = session.amount_total;
   await Booking.create({ tour, user, price });
 };
 
